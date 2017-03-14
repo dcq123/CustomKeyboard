92dd23064db5a1bfdc4f588e3f87ba1094c8a5b1
dingchangqing <qing_soft@sina.cn>
2017-03-14 21:45:30 +0800

    create other lib;

---
+++ otherlib/src/main/java/com/soft/qing/otherlib/OtherView.java
@@ -0,0 +1,25 @@
+package com.soft.qing.otherlib;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by dingchangqing on 2017/3/14.
+ */
+
+public class OtherView extends View {
+    public OtherView(Context context) {
+        super(context);
+    }
+
+    public OtherView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OtherView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
